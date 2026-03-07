a4cecd16ef00bd01e8cf8c181d3102939163212c
John Ferlito <johnf@inodes.org>
2026-03-07 14:13:58 +1100

    chore: fix formatting

--- scripts/update-manifest-version.mjs
+++ scripts/update-manifest-version.mjs
@@ -1,3 +1,4 @@
+import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 
 const version = process.argv[2];
@@ -6,6 +7,7 @@ const manifestPath = 'custom_components/actron_air_esphome/manifest.json';
 const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
 manifest.version = version;
 fs.writeFileSync(manifestPath, `${JSON.stringify(manifest, null, 2)}\n`);
+execSync(`npx biome check --write ${manifestPath}`, { stdio: 'inherit' });
 
 const packagePath = 'package.json';
 const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
@@ -15,3 +17,4 @@ fs.writeFileSync(packagePath, `${JSON.stringify(pkg, null, 2)}\n`);
 const constPath = 'src/const.ts';
 const constContents = fs.readFileSync(constPath, 'utf8');
 fs.writeFileSync(constPath, constContents.replace(/CARD_VERSION\s*=\s*'[^']*'/, `CARD_VERSION = '${version}'`));
+execSync(`npx biome check --write ${constPath}`, { stdio: 'inherit' });
