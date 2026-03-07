74214e373b0e2b3b7784034fa1a8bd0430ee43a5
John Ferlito <johnf@inodes.org>
2026-03-07 13:49:27 +1100

    feat: update version script to also set package.json and CARD_VERSION

--- scripts/update-manifest-version.mjs
+++ scripts/update-manifest-version.mjs
@@ -2,7 +2,16 @@ import fs from 'node:fs';
 
 const version = process.argv[2];
 
-const path = 'custom_components/actron_air_esphome/manifest.json';
-const manifest = JSON.parse(fs.readFileSync(path, 'utf8'));
+const manifestPath = 'custom_components/actron_air_esphome/manifest.json';
+const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
 manifest.version = version;
-fs.writeFileSync(path, `${JSON.stringify(manifest, null, 2)}\n`);
+fs.writeFileSync(manifestPath, `${JSON.stringify(manifest, null, 2)}\n`);
+
+const packagePath = 'package.json';
+const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+pkg.version = version;
+fs.writeFileSync(packagePath, `${JSON.stringify(pkg, null, 2)}\n`);
+
+const constPath = 'src/const.ts';
+const constContents = fs.readFileSync(constPath, 'utf8');
+fs.writeFileSync(constPath, constContents.replace(/CARD_VERSION\s*=\s*'[^']*'/, `CARD_VERSION = '${version}'`));
